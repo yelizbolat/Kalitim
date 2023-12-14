5e0eddf50f40d6997741c904dfa3a8ea82e253c3
yeliz601 <yeliz601@BPC11>
2023-12-14 14:51:33 +0300

    Proje dosyası ekle.

---
+++ Kalitim/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogretmen ogr = new Ogretmen
+            {
+                Ad = "Ahmet",
+                Soyad = "Öz",
+                Brans = "Matematik"
+            };
+            Ogretmen ogretmen = new Ogretmen
+            {
+                Ad = "Ahmet",
+                Soyad = "Öz",
+                Brans = "Matemati"
+            };
+            OkulPersoneli per = ogr; // !!!
+            Console.WriteLine(per.Ad);
+        }
+    }
+    public class OkulPersoneli
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    public class Ogretmen : OkulPersoneli
+    {
+        public string Brans { get; set; }
+    }
+}
